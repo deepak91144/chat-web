6e432eeaa9844d36db30bfaa7ee365501475e615
deepak <deepak_dibyajyoti@hotmail.com>
2024-10-20 11:20:06 +0530

    fix: remove strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { CssBaseline } from "@mui/material";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>
   <HelmetProvider>
     <CssBaseline />
     <App />
   </HelmetProvider>
-  // </StrictMode>
 );
